cce63300c7402a6d341cbceec366c61fed5e3494
YeheonAhn <wngjs42@gmail.com>
2026-05-12 13:02:50 +0900

    2D세팅,캐릭터이동,타이틀,카메라플레이어추적
    2D세팅,캐릭터이동,타이틀,카메라플레이어추적

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnClickQuit()
+    {
+        // 에디터에서는 작동 안함, 빌드에서만 작동!
+        Application.Quit();
+    }
+}
